c0b00beb52a90e2b8a0d66482b5a674a37d1e030
Parker <parker.arneson@gmail.com>
2022-09-16 15:22:06 -0400

    Creating options

--- App/App.py
+++ App/App.py
@@ -7,11 +7,6 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-
-}
-
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -23,10 +18,14 @@ def incoming_sms():
 
     # Determine the right reply for this message
     if body != None:
-        response = "\n\nTitle Here"
-        response += "\n1. Option 1"
-        response += "\n2. Option 2"
-        response += "\n3. Option 3"
+        response = "\nTechie"
+        response += "\nCentral Hub for teching tech literacy.\nWhat do you need help with?"
+        response += "\n1. Setting Up Accounts"
+        response += "\n2. Password Management"
+        response += "\n3. Managing your Storage"
+        response += "\n4. Verifying Emails"
+        response += "\n5. Downloading an App"
+        response += "\n6. Creating a Reminder"
         resp.message(response)
     else:
         resp.message("\nI didnt get that.")
